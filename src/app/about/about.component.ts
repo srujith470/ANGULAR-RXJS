578d594d01f7a1f36e21ed3b599a2ed001af42bd
srujith470 <srujith.reddy2012@gmail.com>
2019-01-17 01:39:03 -0500

    OBSERVANLE CONTRACT

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,9 +16,17 @@ export class AboutComponent implements OnInit {
     const interval$ = timer(4000,1000); // create observable
     const click$ = fromEvent(document, 'click'); // create observable
 
-    interval$.subscribe(val => console.log("stream1:" + val)); // subscribe observable
-    interval$.subscribe(val => console.log("stream2:" + val)); // subscribe observable
-    click$.subscribe(evt => console.log(evt)); // subscribe observable
+    const subscription = interval$.subscribe(val => console.log("stream1:" + val)); // subscribe observable
+    
+    setTimeout(() => {
+        subscription.unsubscribe() 
+    }, 11000); // unsubscribe observable
+    
+    click$.subscribe(
+      evt => console.log(evt),
+      err => console.log(err),
+      () => console.log("completed")
+      ); // subscribe observable
     }
 
 }
